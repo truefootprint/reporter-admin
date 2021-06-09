06c242ffd68f9da90b7eeeae8769191695141012
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2021-06-09 12:43:40 +0200

    Deploy changes

---
+++ precache-manifest.db3c11d804a95946f8c4ab4bdc53e493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2928e5fc407b59bd924fdd411386958f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cfa9be1ada694f315e7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dfab5404e5fb0f242e11",
+    "url": "/static/js/2.7e863427.chunk.js"
+  },
+  {
+    "revision": "3cfa9be1ada694f315e7",
+    "url": "/static/js/main.fed0bf17.chunk.js"
+  },
+  {
+    "revision": "41428884d1e909e58350",
+    "url": "/static/js/runtime-main.612cc2c4.js"
+  }
+]);
